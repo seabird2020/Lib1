fd30ce44e672a48a5f46aff7fbb5afd134ef97bc
yedongzan@126.com <yedongzan@126.com>
2020-05-14 18:12:18 +0800

    add two source files

---
+++ Lib1/Classes/CommonUtils.h
@@ -0,0 +1,16 @@
+//
+//  CommonUtils.h
+//  SplayGameSDK
+//
+//  Created by Nguyen Duy Khanh on 7/4/19.
+//  Copyright © 2019 DucViet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CommonUtils : NSObject {}
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Lib1/Classes/CommonUtils.m
@@ -0,0 +1,18 @@
+//
+//  CommonUtils.m
+//  SplayGameSDK
+//
+//  Created by Nguyen Duy Khanh on 7/4/19.
+//  Copyright © 2019 DucViet. All rights reserved.
+//
+
+#import "CommonUtils.h"
+#import <UIKit/UIKit.h>
+
+
+@implementation CommonUtils
+{
+    
+}
+
+@end
